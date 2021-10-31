487105974d207ac57936fe0579aa7721cf1e765b
VuTien-creator <tienlv304@gmail.com>
2021-10-31 12:27:43 +0700

    Livewire: Create Cart - Action: Remove, Update

--- app/Http/Livewire/Customer/Page/Cart.php
+++ app/Http/Livewire/Customer/Page/Cart.php
@@ -2,29 +2,32 @@
 
 namespace App\Http\Livewire\Customer\Page;
 
+use App\Facades\Cart as AppFacadesCart;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart as FacadesCart;
 use Livewire\Component;
 
 class Cart extends Component
 {
-    private $cart;
-    private $cartSubTotal;
+    protected $cart;
+    protected $cartSubTotal;
 
+    public $message = '';
     public function mount()
     {
 
-        $cartSubTotal =str_replace( ',', '',FacadesCart::subTotal(0));
-        
-        $this->cartSubTotal = (int)$cartSubTotal ;
+        // $cartSubTotal =str_replace( ',', '',FacadesCart::subTotal(0));
+
+        // $this->cartSubTotal = (int)$cartSubTotal ;
 
 
-        $this->cart = FacadesCart::content();
+        // $this->cart = FacadesCart::content();
 
-        foreach ($this->cart as $product) {
-            $product->image = Product::query()->CustomerGetImageById($product->id);
-        }
+        // foreach ($this->cart as $product) {
+        //     $product->image = Product::query()->CustomerGetImageById($product->id);
+        // }
 
+        $this->updateCart();
 
     }
 
@@ -35,5 +38,36 @@ class Cart extends Component
             'cartSubTotal' => $this->cartSubTotal
         ])
         ->layout('customer.layout');
+    }
+
+    public function updateCart(){
+        $this->cartSubTotal = AppFacadesCart::total();
+        $this->cart = AppFacadesCart::content();
+
+        foreach ($this->cart as $id => $product) {
+            $product['image'] = Product::query()->CustomerGetImageById($id);
+        }
+    }
+
+    public function removeFromCart(string $id): void
+    {
+        // dd($id);
+        AppFacadesCart::remove($id);
+        $this->updateCart();
+        $this->emit('cartCounter');
+    }
+
+    /**
+     * Updates a cart item.
+     *
+     * @param string $id
+     * @param string $action
+     * @return void
+     */
+    public function updateCartItem(string $id, string $action): void
+    {
+        AppFacadesCart::update($id, $action);
+        $this->updateCart();
+        $this->emit('cartCounter');
     }
 }
--- resources/views/livewire/customer/page/cart.blade.php
+++ resources/views/livewire/customer/page/cart.blade.php
@@ -27,33 +27,38 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($cart as $product)
+                                    @foreach ($cart as $id => $product)
                                         <tr>
-
                                             <td class="shoping__cart__item">
-                                                <img src="img/cart/cart-1.jpg" alt="">
-                                                <h5>{{ $product->name }}</h5>
+                                                <img src="{{ $product['image'] }}" alt="{{ $product['name'] }}">
+                                                <a href="{{ route('customer.productDetail', $id) }}">
+                                                    <h5>{{ $product['name'] }}</h5>
+                                                </a>
                                             </td>
                                             <td class="shoping__cart__price">
-                                                {{ number_format($product->price) }} VND
+                                                {{ number_format($product['price']) }} VND
                                             </td>
                                             <td class="shoping__cart__quantity">
-                                                <div class="quantity">
-                                                    <div class="pro-qty">
-                                                        <input type="text" value="{{ $product->qty }}">
-                                                    </div>
-                                                </div>
+                                                {{-- <livewire:customer.feature.update-cart :product='$product'> --}}
+                                                <button
+                                                    class="text-sm p-2 border-2 rounded border-gray-200 hover:border-gray-300 bg-gray-200 hover:bg-gray-300"
+                                                    wire:click="updateCartItem({{ $id }}, 'minus')"> -
+                                                </button>
+                                                {{ $product['quantity'] }}
+                                                <button
+                                                    class="text-sm p-2 border-2 rounded border-gray-200 hover:border-gray-300 bg-gray-200 hover:bg-gray-300"
+                                                    wire:click="updateCartItem({{ $id }}, 'plus')"> +
+                                                </button>
                                             </td>
                                             <td class="shoping__cart__total">
-                                                {{ number_format($product->price * $product->qty) }} VND
+                                                {{ number_format($product['price'] * $product['quantity']) }} VND
                                             </td>
                                             <td class="shoping__cart__item__close">
-                                                <span class="icon_close"></span>
+                                                <span class="icon_close"
+                                                    wire:click='removeFromCart({{ $id }})'></span>
                                             </td>
                                         </tr>
                                     @endforeach
-
-
                                 </tbody>
                             </table>
                         </div>
@@ -63,7 +68,6 @@
                     <div class="col-lg-12">
                         <div class="shoping__cart__btns">
                             <a href="{{ route('customer.shop') }}" class="primary-btn ">CONTINUE SHOPPING</a>
-
                         </div>
                     </div>
                     <div class="col-lg-6">
@@ -73,8 +77,8 @@
                         <div class="shoping__checkout">
                             <h5>Cart Total</h5>
                             <ul>
-                                <li>Subtotal <span>{{ number_format($cartSubTotal) }} VND</span></li>
-                                <li>Total <span>{{ number_format($cartSubTotal) }} VND</span></li>
+                                <li>Subtotal <span>{{ $cartSubTotal }} VND</span></li>
+                                <li>Total <span>{{ $cartSubTotal }} VND</span></li>
                             </ul>
                             <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
                         </div>
