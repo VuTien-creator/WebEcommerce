b3dce06ad5ddbbc81237c5926c9492a8426bdee3
VuTien-creator <tienlv304@gmail.com>
2021-11-04 09:56:23 +0700

    Livewire: Install Package PowerGrid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         @livewireStyles
+        @powerGridStyles
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
@@ -42,5 +43,6 @@
         @stack('modals')
 
         @livewireScripts
+        @powerGridScripts
     </body>
 </html>
