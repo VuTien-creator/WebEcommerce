edfb32b62fe6c104bac92409b48d7351f99e87fb
VuTien-creator <tienlv304@gmail.com>
2021-11-11 09:08:54 +0700

    Middleware: Create middleware admin

---
+++ app/Http/Middleware/Admin/AdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware\Admin;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::user()->role->role != 'admin'){
+            return abort(404);
+        }
+        
+        return $next($request);
+    }
+}
